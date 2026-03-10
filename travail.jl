4e20e5016b99aa66d74400056cfbc02b29c87837
ametrinee <herrlich0703@gmail.com>
2026-03-09 22:40:59 -0400

    Update.jl

--- travail.jl
+++ travail.jl
@@ -123,8 +123,6 @@ axislegend(ax1)
 tightlimits!(ax1)
 f1
 
-save("travail-sans-feu.png", f1)
-
 ## Visualisation de scénario 2 : avec intervention
 f2 = Figure();
 ax2 = Axis(f2[1, 1], title="Avec feu contrôlé", xlabel="Nb. générations", ylabel="Nb. parcelles")
@@ -136,8 +134,6 @@ end
 axislegend(ax2)
 tightlimits!(ax2)
 f2
-
-save("travail-avec-feu.png", f2)
 
 # # Présentation des résultats
 
